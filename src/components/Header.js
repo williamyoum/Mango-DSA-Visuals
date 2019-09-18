749110f6f93d4e370cc304f231f2eb59384847f7
Daniel Yoo <yoodan@amazon.com>
2019-09-17 22:57:42 -0700

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import mango from '../mango.svg'
 
 function Header() {
+  const navStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    height: '60px',
+    padding: '10px',
+    boxShadow: '0 4px 2px -2px gray',
+    marginBottom: '10px'
+  }
+
+  const logoStyle = {
+    height: '100%',
+    marginRight: '20px'
+  }
+
+  const titleStyle = {
+    fontWeight: 'bold',
+    fontSize: '1.5rem'
+  }
+
   return (
-    <div>
-      HEADER
-    </div>
+    <nav style={navStyle}>
+      <img style={logoStyle} src={mango} alt="Logo" />
+      <div style={titleStyle}>Mango Algorithm Visualizer</div>
+    </nav>
   );
 }
 
