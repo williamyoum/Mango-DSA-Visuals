1d61797d84678df82329236a989f0164db21032b
Will <will.youm@gmail.com>
2019-09-29 18:43:04 -0400

    mango deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Footer from '../components/Footer.js'
 // import Dropdown from '../components/Shared/Dropdown.js';
-import Header from '../components/Header.js';
+// import Header from '../components/Header.js';
 
 class Home extends React.Component {
   render() {
