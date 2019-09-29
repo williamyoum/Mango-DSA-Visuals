743a6e399ed58045f5c647177e87591f5c57591f
Will <will.youm@gmail.com>
2019-09-29 18:36:11 -0400

    Updates

---
+++ precache-manifest.0218bb18ea35d63363f5a0e39c8a33f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5946fc0035d54b581d42c2eef0aefbdd",
+    "url": "williamyoum.github.io/mango.git/index.html"
+  },
+  {
+    "revision": "6ccb1e898a3924d764df",
+    "url": "williamyoum.github.io/mango.git/static/css/main.36ec1a24.chunk.css"
+  },
+  {
+    "revision": "87b0d1d69b88c2652a58",
+    "url": "williamyoum.github.io/mango.git/static/js/2.0334500e.chunk.js"
+  },
+  {
+    "revision": "6ccb1e898a3924d764df",
+    "url": "williamyoum.github.io/mango.git/static/js/main.7531150f.chunk.js"
+  },
+  {
+    "revision": "d734791758452d025be5",
+    "url": "williamyoum.github.io/mango.git/static/js/runtime~main.fdfb27b5.js"
+  },
+  {
+    "revision": "b9f255f877cf83e7a2e840d5670919ba",
+    "url": "williamyoum.github.io/mango.git/static/media/mango.b9f255f8.svg"
+  }
+]);
